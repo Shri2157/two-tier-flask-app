a5b8b3abe963c185101e43e588166f8a0b404447
Ubuntu <ubuntu@ip-172-31-87-201.ec2.internal>
2024-09-11 11:35:37 +0000

    Pushing dockerfile and docker-compose.yml

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+From python:3.9-slim
+Workdir /app
+Run apt-get update &&  apt-get upgrade -y  && \ 
+    apt-get install -y gcc default-libmysqlclient-dev pkg-config && \
+    rm -rf /vat/lib/apt/lists/*
+COPY requirements.txt .
+run pip install mysqlclient
+run pip install --no-cache-dir -r requirements.txt
+copy . .
+CMD  ["python" , "app.py"] 
--- app.py
+++ app.py
@@ -5,25 +5,29 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 # Configure MySQL from environment variables
-app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST', 'localhost')
-app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', 'default_user')
-app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', 'default_password')
-app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB', 'default_db')
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST', 'mysql')
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', 'root')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', 'admin')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB','mydb')
 
 # Initialize MySQL
 mysql = MySQL(app)
 
 def init_db():
     with app.app_context():
-        cur = mysql.connection.cursor()
-        cur.execute('''
-        CREATE TABLE IF NOT EXISTS messages (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            message TEXT
-        );
-        ''')
-        mysql.connection.commit()  
-        cur.close()
+        try:
+            cur = mysql.connection.cursor()
+            cur.execute('''
+            CREATE TABLE IF NOT EXISTS messages (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                message TEXT
+            );
+            ''')
+            mysql.connection.commit()
+            cur.close()
+        except Exception as e:
+            print(f"Error initializing the database: {e}")
+
 
 @app.route('/')
 def hello():
